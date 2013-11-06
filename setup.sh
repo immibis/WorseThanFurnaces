02892466b0ab03b09d70908d2e8b8214e8467fd9
immibis <immibis@gmail.com>
2013-11-06 19:39:02 +1300

    Updated setup.sh - after patching, convert all source files to unix line endings and back again if on windows. Otherwise msys patch can leave files with inconsistent line endings.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,21 @@
 
 GITDIR="`pwd`"
 
+IS_WINDOWS=
+if uname | grep CYGWIN; then
+	IS_WINDOWS=cygwin
+else
+	if uname | grep MINGW32; then
+		IS_WINDOWS=msys
+	fi
+fi
+
+IS_HD2U=
+if dos2unix --version 2>&1 | head -n1 | grep hd2u; then
+	IS_HD2U=yes
+fi
+	
+
 if [ -e ../non-redist/mcp811.zip ]; then
 #	if [ `md5sum ../non-redist/mcp811.zip | cut '-d ' -f1` != 2d7a759309b5cc10ca29caa0b10f3bfc ]; then
 #		echo mcp811.zip md5 not what was expected
@@ -16,7 +31,8 @@ if [ -e ../non-redist/mcp811.zip ]; then
 		fi
 		if [ -e ../mcp/jars/saves ]; then
 			SAVESDIR="`pwd`/../temp-setup-saves"
-			mv ../mcp/jars/saves "$SAVESDIR"
+			mkdir "$SAVESDIR"
+			mv "../mcp/jars/saves"/{saves,config} "$SAVESDIR"
 		fi
 		rm -rf ../mcp
 	fi
@@ -30,8 +46,8 @@ if [ -e ../non-redist/mcp811.zip ]; then
 	mv Start.java conf/patches
 
 	# hack for cygwin
-	cmd "/c decompile.bat
- < NUL" || ./decompile.sh < /dev/null
+	cmd "/c decompile.bat < NUL
+" || ./decompile.sh < /dev/null
 
 	if [ ! -e src/minecraft ]; then
 		echo Decompile failed. Exiting.
@@ -42,7 +58,7 @@ if [ -e ../non-redist/mcp811.zip ]; then
 		if [ ! -e jars ]; then
 			mkdir jars
 		fi
-		mv "$SAVESDIR" jars/saves
+		mv "$SAVESDIR/*" jars
 	fi
 
 	cp -rf src src-base
@@ -50,6 +66,20 @@ if [ -e ../non-redist/mcp811.zip ]; then
 	BASEEDITS="$GITDIR/baseedits.patch"
 	if [ -e "$BASEEDITS" ]; then
 		patch -p1 -u < "$BASEEDITS"
+
+		# msys patch converts touched files to unix line endings,
+		# which is undesirable, so use MCP's patch if possible
+
+		# AND. cygwin and msys use different incompatible versions of dos2unix!
+		if [ ! -z $IS_WINDOWS ]; then
+			if [ ! -z $IS_HD2U ]; then
+				find -type f | xargs dos2unix --d2u
+				find -type f | xargs dos2unix --u2d
+			else
+				find -type f | xargs dos2unix
+				find -type f | xargs unix2dos
+			fi
+		fi
 	fi
 else
 	if [ ! -e ../non-redist ]; then
