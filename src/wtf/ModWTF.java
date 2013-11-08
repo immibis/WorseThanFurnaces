a3bc9ead0165142a5a40109e5f4990e97abcb934
immibis <immibis@gmail.com>
2013-11-08 14:48:47 +1300

    Added block sensor block.

--- src/wtf/ModWTF.java
+++ src/wtf/ModWTF.java
@@ -11,6 +11,6 @@ import wtfml.WTFML;
 public class ModWTF extends Mod {
 	@Override
 	public void initBlocks() {
-		WTFML.addBlock("wtf:", new Block(Material.pumpkin) {{func_2_Block_BlockF(0.2F);func_1_Block_BlockString("bed");func_0_Block_Block();func_2_Block_BlockString("stone");func_0_Block_BlockCreativeTab(CreativeTab.brewing);}});
+		WTFML.addBlock("wtf:block-sensor", new BlockBlockSensor());
 	}
 }
